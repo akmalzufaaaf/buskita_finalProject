d1027ef37f4fe1593dca29930c9e423337483056
akmalzufaaaf <141655864+akmalzufaaaf@users.noreply.github.com>
2023-11-12 21:28:12 +0700

    Update TicketShow.py
    handling error jika id yang dimasukkan tidak sesuai dan melakukan beberapa perbaikan format pada e-ticket.

--- TicketShow.py
+++ TicketShow.py
@@ -8,7 +8,14 @@ class TicketShow:
         with open("passengerData.csv",'r+',newline="") as f:
             r =  csv.reader(f)
             data = list(r)
-            id = int(input("Masukkan Booking ID  :"))
+            while True:
+                try:
+                    id = int(input("Masukkan Booking ID  :"))
+                    break
+                except:
+                    print("Booking ID hanya berupa angka!")
+                    continue
+                
             for i in data:
                 if id == int(i[0]):
                     for j in i:
@@ -19,18 +26,18 @@ class TicketShow:
         print("                                  BUSKITA                                     ")
         print("------------------------------------------------------------------------------")
         print()
-        print(" e_Ticket :", "Nagpur Address              : Hingna Road Priyadarshini T-Point")
-        print("           ", "Phone No\Mob No             : 8000800088,8888880000            ")
+        print(" e_Ticket :", "Alamat Kantor               : Jl. Jalan No. 100                ")
+        print("           ", "Nomor Telepon               : 081292102121929                  ")
         print()
-        print("",bln[3],"------------->",bln[4],"            ","        Passenger Id:",bln[0])
+        print("",bln[3],"------------->",bln[4],"             ","        Passenger Id:",bln[0])
         print()
-        print(" Passenger Name :", bln[1],"              ","Number of Passenger :",bln[2])
+        print(" Nama Penumpang :", bln[1],"                   ","        Jumlah Penumpang :",bln[2])
         print("______________________________________________________________________________")
         print()
-        print(" Date of Booking :",bln[5],"              ","Seat No :",bln[6],"              ")
+        print(" Tanggal Pemesanan :",bln[5],"            ","             Kursi Nomor :",bln[6],"          ")
         print()
-        print(" Bus Type :       ",bln[7],"                                                           ")
-        print(" Bus Fare :       ",bln[8],"                                                           ")
+        print(" Tipe Bus :       ",bln[7],"                                                           ")
+        print(" Harga    :       ",bln[8],"                                                           ")
         print()
         print("------------------------------------------------------------------------------")
                 
