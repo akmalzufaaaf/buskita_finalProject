f0fd91628ccdc45860db68a81c54eaf6d56077ca
akmalzufaaaf <141655864+akmalzufaaaf@users.noreply.github.com>
2023-11-16 21:22:50 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -10,8 +10,8 @@ print("-----------------------------------------------------")
 print()
 
 def start(): #called function
-    print("1. Buat Akun Baru :")
-    print("2. Login       :")
+    print("1. Buat Akun Baru")
+    print("2. Login       ")
     print()
     adminObj = Admin()
     ch = int(input("Pilih sesuai dengan nomornya: "))
@@ -19,20 +19,30 @@ def start(): #called function
     if ch == 1:
         #admin class object creation
         adminObj.adminRegistration()
-
+        start()
+        
     if ch == 2:
         
         adminObj.adminLogin()
 
         print()
-        print("1. Passenger Registration :")
-        print("2. Show Ticket            :")
+        print("1. Pesan tiket")
+        print("2. Lihat tiket")
         print()
         ch = int(input("Pilih opsi: "))
         if ch == 1:
             pd_obj = PassengerDataCsv()
             pd_obj.getPassengerInfo()
             pd_obj.saveInfo()
+
+            print("Apakah kamu ingin memesan tiket lagi [yes/no]? ") 
+            y = input("").lower()
+            if y == "yes":
+                pd_obj.getPassengerInfo()
+            elif y == "no":
+                start()
+            else:
+                print("Pilih yes/no!")  
         elif ch ==2:
             obj = TicketShow()
             obj.ticketShow()
